025f6d6184402002d939fda328376704281028a0
Dimarik <krasnodar93@mail.ru>
2022-04-03 09:32:05 +0300

    0304-test

--- puzzle/src/main/java/ru/job4j/puzzle/Win.java
+++ puzzle/src/main/java/ru/job4j/puzzle/Win.java
@@ -2,7 +2,32 @@ package ru.job4j.puzzle;
 
 public class Win {
     public static boolean check(int[][] board) {
-        boolean rsl = true;
-        return rsl;
+
+        for (int i = 0; i < board.length; i++) {
+            if (zeroLine(board, true, i)) {
+                return true;
+            }
+        }
+        for (int j = 0; j < board[0].length; j++) {
+            if (zeroLine(board, false, j)) {
+                return true;
+            }
+        }
+        return false;
     }
+
+    public static boolean zeroLine(int[][]board, boolean rowLine, int number)
+    {
+        for (int i = 0; i < board.length; i++) {
+            int row = rowLine ? number : i;
+            int cell = !rowLine ? number : i;
+            if (board[row][cell] != 1) {
+                return  false;
+            }
+        }
+        return  true;
+
+    }
+
+
 }
