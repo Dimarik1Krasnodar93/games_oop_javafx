a41852d9bf3a2629fa8c01e83df3e884acab0ee2
Dimarik <krasnodar93@mail.ru>
2022-04-06 14:31:36 +0300

    0604-win-1431

--- puzzle/src/main/java/ru/job4j/puzzle/Win.java
+++ puzzle/src/main/java/ru/job4j/puzzle/Win.java
@@ -4,25 +4,36 @@ public class Win {
     public static boolean check(int[][] board) {
 
         for (int i = 0; i < board.length; i++) {
-            if (zeroLine(board, true, i) || zeroLine(board, false, i)) {
+            if (monoVertical(board, i) || monoHorizontal(board, i)) {
                 return true;
             }
         }
         return false;
     }
 
-    public static boolean zeroLine(int[][]board, boolean rowLine, int number)
-    {
+    public static boolean monoVertical(int[][] board, int column) {
+        boolean result = true;
         for (int i = 0; i < board.length; i++) {
-            int row = rowLine ? number : i;
-            int cell = !rowLine ? number : i;
-            if (board[row][cell] != 1) {
-                return  false;
+            if (board[i][column] != 1) {
+                result = false;
+                break;
             }
         }
-        return  true;
-
+        return result;
     }
+
+    public static boolean monoHorizontal(int[][] board, int row) {
+        boolean result = true;
+        for (int i = 0; i < board[row].length; i++) {
+            if (board[row][i] != 1) {
+                result = false;
+                break;
+            }
+        }
+        return result;
+    }
+
+
 
 
 }
